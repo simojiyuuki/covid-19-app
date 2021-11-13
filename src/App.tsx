0f74efe59028400541e95511436612cac690847e
simojiyuuki <simoji97@gmail.com>
2021-11-14 02:45:51 +0900

    creat-react-app covid-19-app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,45 +14,25 @@ function App() {
         </p>
         <span>
           <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
             Redux Toolkit
           </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <span> and </span>
+          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
             React Redux
           </a>
         </span>
       </header>
     </div>
   );
-}
+};
 
 export default App;
